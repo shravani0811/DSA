a075641aa9a99e1055e8eeb375facb85859c1bd6
shravani0811 <shravani0811@gmail.com>
2024-07-08 13:57:16 -0700

    from trees

---
+++ Trees/Importants Binary tree/226. Invert Binary Tree sol 2.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        # if not root:
+        #     return None
+        # temp=root.left
+        # root.left=root.right
+        # root.right=temp
+
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
+
+        # return root
+
+        def invert(root):
+            if not root:
+                return None
+            temp=root.left
+            root.left=invert(root.right)
+            root.right=invert(temp)
+            return root
+        return invert(root)
+        
+        
